9b6ee9484b4fdc3f352a34a95cbbe49c169722ab
tayabatariq <168851346+tayabatariq@users.noreply.github.com>
2024-12-18 21:52:28 +0500

    inital style

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -100,100 +100,112 @@ function App() {
     getTodos();
   }, []);
 
-  
+
 
   return (
     <>
-      <div className="text-3xl p-5 text-yellow-100">Mongo Memberlist</div>
-      <div className="p-5">
-        <form onSubmit={handleSubmit} className="flex gap-5 ">
-          <input
-            value={input}
-            onChange={(e) => setInput(e.target.value)}
-            className="bg-transparent p-1 text-lg border"
-            type="text"
-          />
-          {todos.some((item) => item.isChecked) ? (
-            <>
-            <button
-              type="button"
-              onClick={() => handleDeleteAll(todos)}
-              className="border px-5"
-            >
-              Delete Selected
-            </button>
-            <button
-              type="button"
-              onClick={() => toggleCheckboxes()}
-              className="border px-5"
-            >
-              Unselect All
-            </button>
-            </>
-          ) : (
-            <button type="submit" className="border px-5">
-              Save
-            </button>
-          )}
-        </form>
-        <ul>
-          {todos.map((todo, index) => {
-            return (
-              <div key={todo._id} className="flex gap-5 py-2">
-                {todo.isEditable ? (
-                  <>
-                    <input
-                      value={todo.name || ""}
-                      onChange={(e) =>
-                        handleEditChange(todo._id, e.target.value)
-                      }
-                      className="bg-transparent p-1 text-lg border"
-                      type="text"
-                    />
-                    <button
-                      onClick={() => toggleEdit(todo._id)}
-                      className="border rounded-full bg-red-50 text-black text-sm px-5"
-                    >
-                      Cancel
-                    </button>
-                    <button
-                      onClick={() => handleEdit(todo._id, todo.name)}
-                      className="border rounded-full bg-red-50 text-black text-sm px-5"
-                    >
-                      Update
-                    </button>
-                  </>
-                ) : (
-                  <>
-                    <label className="todo-label" htmlFor={todo._id}>
+      <div className="flex  flex-col justify-center items-center  w-full ">
+
+        <div className="text-6xl uppercase  font-bold p-32 max-sm:h-64 max-md:h-96 max-sm:text-4xl max-sm:p-20 max-lg:text-5xl max-lg:p-28 text-center bg-[url(bg1.jpg)] bg-cover bg-center h-60 w-full  text-white">Capture Your Thoughts and Never Miss a Detail</div>
+        <div className="p-5 bg-green-800 m-7 rounded-lg h-full ">
+          <form onSubmit={handleSubmit} className="flex gap-5 ">
+            <input
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+              className=" p-1 w-full  border-2 text-black  rounded-md"
+              type="text"
+              placeholder="Type Here Messege"
+            />
+            {todos.some((item) => item.isChecked) ? (
+              <>
+                <button
+                  type="button"
+                  onClick={() => handleDeleteAll(todos)}
+                  className="border px-5"
+                >
+                  Delete Selected
+                </button>
+                <button
+                  type="button"
+                  onClick={() => toggleCheckboxes()}
+                  className="border px-5"
+                >
+                  Unselect All
+                </button>
+              </>
+            ) : (
+              <button type="submit" className="border px-5">
+                Save
+              </button>
+            )}
+          </form>
+          <ul>
+            {todos.map((todo, index) => {
+              return (
+                <div key={todo._id} className="flex gap-5 py-2 items-start max-sm:flex-col justify-start">
+                  {todo.isEditable ? (
+                    <>
+
                       <input
-                        type="checkbox"
-                        onChange={() => toggleCheckboxes(todo._id)}
-                        checked={todo.isChecked}
-                        id={todo._id}
+                        value={todo.name || ""}
+                        onChange={(e) =>
+                          handleEditChange(todo._id, e.target.value)
+                        }
+                        className="bg-transparent p-1 text-lg border"
+                        type="text"
                       />
-                      <span className="checkmark"></span>
-                      {todo.name}
-                    </label>
-                    <button
-                      onClick={() => handleDelete(todo._id)}
-                      className="border rounded-full bg-red-50 text-black text-sm px-5"
-                    >
-                      Delete
-                    </button>
-                    <button
-                      onClick={() => toggleEdit(todo._id)}
-                      className="border rounded-full bg-red-50 text-black text-sm px-5"
-                    >
-                      Edit
-                    </button>
-                  </>
-                )}
-              </div>
-            );
-          })}
-        </ul>
+                        <button
+                          onClick={() => toggleEdit(todo._id)}
+                          className="border rounded-lg bg-red-50 text-black text-sm px-5"
+                        >
+                          Cancel
+                        </button>
+                        <button
+                          onClick={() => handleEdit(todo._id, todo.name)}
+                          className="border rounded-lg bg-red-50 text-black text-sm px-5"
+                        >
+                          Update
+                        </button>
+
+
+                    </>
+                  ) : (
+                    <>
+                      <label className="todo-label" htmlFor={todo._id}>
+                        <input
+                          type="checkbox"
+                          onChange={() => toggleCheckboxes(todo._id)}
+                          checked={todo.isChecked}
+                          id={todo._id}
+                        />
+                        <span className="checkmark"></span>
+                        {todo.name}
+                      </label>
+                      <div className=" w-full flex gap-4 ">
+
+                      <button
+                        onClick={() => handleDelete(todo._id)}
+                        className="border rounded-sm bg-red-50 text-black text-sm px-5"
+                      >
+                        Delete
+                      </button>
+                      <button
+                        onClick={() => toggleEdit(todo._id)}
+                        className="border rounded-sm bg-red-50 text-black text-sm px-5"
+                      >
+                        Edit
+                      </button>
+                      </div>
+
+                    </>
+                  )}
+                </div>
+              );
+            })}
+          </ul>
+        </div>
       </div>
+
     </>
   );
 }
